8ecbece6c1e1aee31cce6d8db4d6fb41d56357ce
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-05-01 16:34:31 +0530

    alt- err4o4.jsx file

--- src/components/Err404.jsx
+++ src/components/Err404.jsx
@@ -5,8 +5,8 @@ function Err404() {
   return (
     <>
     <div className='min-h-screen flex justify-center items-center flex-col '>
-      <h1 className='text-5xl md:text-8xl text-red-500'>404! </h1>
-      <h1 className='text-5xl md:text-8xl '> page not found</h1>
+      <h1 className='text-4xl md:text-8xl text-red-500'>404! </h1>
+      <h1 className='text-4xl md:text-8xl '> page not found</h1>
       <Link to={'/'} className='border hover:bg-black duration-200 py-3 px-5 mt-3 rounded-full bg-secondary text-white cursor-pointer '>Back to home</Link>
     </div>
     </>
