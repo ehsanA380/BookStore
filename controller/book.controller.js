d225ed81e86f5d4895033a2fbbedcdeb886002ca
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-05-03 18:27:19 +0530

    alt - boo.controller.js file

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -2,7 +2,9 @@ import Book from "../model/Book.model.js";
 
 export const getBook = async(req,res)=>{
     try {
-        const book= await Book.find();
+        const options = { timeout: 30000 };
+        const book= await Book.find({}, null, options);
+        // const result = await Book.find({}, null, options);
         res.status(200).json(book)
         
         
